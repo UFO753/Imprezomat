7538c58c6995358f51d30839034c13527706dd64
UFO753 <ufoufo753@gmail.com>
2023-11-07 19:49:57 +0100

    Added active status change for navbar

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,45 @@
 function serviceproviderShortText(description) {
-  console.log(description);
   document.write(description.slice(0, 200) + "...");
 }
 
 function serviceproviderCheckVerified(verified) {
   console.log(verified);
   if (verified == "yes") {
-    console.log(verified);
     document.write('<h6 class= "bg-success" >Zweryfikowany</h6>');
   } else if (verified == "no") {
     document.write('<h6 class= "bg-danger" >Oczekuje na weryfikację</h6>');
   } else {
     document.write('<h6 class= "bg-warning" >Błąd weryfikacji</h6>');
+  }
+}
+
+function ChangeActiveNavbar(toChange) {
+  let tempActive = document.getElementsByClassName("active");
+  tempActive[0].classList.remove("active");
+  let tempNewActive = document.getElementById(toChange);
+  tempNewActive.classList.add("active");
+}
+
+var currentUrl = window.location.pathname;
+var currentUrlCorrect = currentUrl.substring(1);
+if (window.location) {
+  switch (currentUrlCorrect) {
+    case "about":
+      ChangeActiveNavbar("navabout");
+      break;
+    case "login":
+      ChangeActiveNavbar("navlogin");
+      break;
+    case "register":
+      ChangeActiveNavbar("navregister");
+      break;
+    case "calendar":
+      ChangeActiveNavbar("navcalendar");
+      break;
+    case "serviceproviders":
+      ChangeActiveNavbar("navserviceproviders");
+      break;
+    default:
+    // code block
   }
 }
